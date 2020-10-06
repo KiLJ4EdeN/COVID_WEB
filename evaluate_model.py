582c1ff33cce3837e92da6b03f9bd5053b04f31e
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-10-06 18:14:42 +0330

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,3 +1,4 @@
+# Import Dependencies.
 from sklearn.svm import SVC, NuSVC
 from sklearn.model_selection import cross_val_score, StratifiedKFold, cross_val_predict
 from sklearn.metrics import confusion_matrix
@@ -6,16 +7,23 @@ import time
 from warnings import simplefilter
 from scipy.io import loadmat
 
+# Calculate time.
 s_time = time.time()
+# Load the extracted features.
 FV = loadmat('features.mat')
 X = FV['data']
 Y = FV['labels']
 Y = Y.transpose()
+
+# Create the Classifier.
 clf = NuSVC(nu=0.4, kernel='rbf', gamma=0.009876939713502824, shrinking=True, tol=0.00001,
           max_iter=176, random_state=1, class_weight='balanced', probability=True)
 simplefilter(action='ignore')
+
+# Perform Cross Validation to Measure the Performance.
 cv = StratifiedKFold(n_splits=10, shuffle=True, random_state=603,)
 
+# TODO: Probably not the most optimal way to do this.
 accuracy = cross_val_score(clf, X, Y, cv=cv)
 scores = cross_val_score(clf, X, Y, cv=cv, scoring='roc_auc')
 recall = cross_val_score(clf, X, Y, cv=cv, scoring='recall')
